5cbd333c873b882586c67bebd17e886665863615
Ruslan Abkadirov <rusabk@yandex.ru>
2020-01-17 04:58:55 +0300

    vue-router configured

--- src/router/index.js
+++ src/router/index.js
@@ -12,16 +12,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/users/:id',
+    component: Profile
   },
   {
-    path:'/profile',
-    component: Profile
+    path:'/users',
+    component: Home
   }
 ]
 
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,21 +2,21 @@
     <div>
         <v-row class="text-left">
             <v-col cols="2">
-                <img src="https://randomuser.me/api/portraits/men/1.jpg" style="max-width: 100%">
+                <img :src="'https://randomuser.me/api/portraits/men/' + $route.params.id + '.jpg'" style="max-width: 100%">
             </v-col>
             <v-col cols="10" class="text-left">
-                <h1>Leanne Graham</h1>
+                <h1>{{userData.name}}</h1>
                 <p>
-                    Веб-сайт: <a href="http://hildegard.org" target="_blank">hildegard.org</a>
+                    Веб-сайт: <a :href="'http://'+ userData.website" target="_blank">{{userData.website}}</a>
                 </p>
                 <p>
-                    E-mail: <a href="mailto:Sincere@april.biz">Sincere@april.biz</a>
+                    E-mail: <a :href="'mailto:' + userData.email">{{userData.email}}</a>
                 </p>
                 <p>
-                    Город: Gwenborough
+                    Город: {{userData.address.city}}
                 </p>
                 <p>
-                    Место работы: Romaguera-Crona
+                    Место работы: {{userData.company.name}}
                 </p>
             </v-col>
         </v-row>
@@ -25,17 +25,20 @@
 
         <div class="mt-5">
             <h2 class="text-left mb-7">Публикации</h2>
-            <v-card max-width="1000px" class="text-left pa-3 mb-8">
+
+            <v-card 
+                v-for="post in posts" :key="post.id"
+                max-width="1000px" class="text-left pa-3 mb-8" >
                 <v-list-item>
                     <v-list-item-avatar color="grey">
-                        <img src="https://randomuser.me/api/portraits/men/1.jpg">
+                        <img :src="'https://randomuser.me/api/portraits/men/' + post.userId + '.jpg'">
                     </v-list-item-avatar>
                     <v-list-item-content>
-                        <v-list-item-title>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</v-list-item-title>
-                        <v-list-item-subtitle>by Leanne Graham</v-list-item-subtitle>
+                        <v-list-item-title>{{post.title}}</v-list-item-title>
+                        <v-list-item-subtitle>by {{userData.name}}</v-list-item-subtitle>
                     </v-list-item-content>
                 </v-list-item>
-                <v-card-text>quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto</v-card-text>
+                <v-card-text>{{post.body}}</v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
 
@@ -52,118 +55,52 @@
                     </v-btn>
                 </v-card-actions>
             </v-card>
-            
-            <v-card max-width="1000px" class="text-left pa-3 mb-8">
-                <v-list-item>
-                    <v-list-item-avatar color="grey">
-                        <img src="https://randomuser.me/api/portraits/men/1.jpg">
-                    </v-list-item-avatar>
-                    <v-list-item-content>
-                        <v-list-item-title>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</v-list-item-title>
-                        <v-list-item-subtitle>by Leanne Graham</v-list-item-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-card-text>quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto</v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
 
-                    <v-btn icon>
-                        <v-icon>mdi-heart</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                        <v-icon>mdi-bookmark</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                        <v-icon>mdi-share-variant</v-icon>
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-            
-            <v-card max-width="1000px" class="text-left pa-3 mb-8">
-                <v-list-item>
-                    <v-list-item-avatar color="grey">
-                        <img src="https://randomuser.me/api/portraits/men/1.jpg">
-                    </v-list-item-avatar>
-                    <v-list-item-content>
-                        <v-list-item-title>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</v-list-item-title>
-                        <v-list-item-subtitle>by Leanne Graham</v-list-item-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-card-text>quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto</v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-
-                    <v-btn icon>
-                        <v-icon>mdi-heart</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                        <v-icon>mdi-bookmark</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                        <v-icon>mdi-share-variant</v-icon>
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-            
-            <v-card max-width="1000px" class="text-left pa-3 mb-8">
-                <v-list-item>
-                    <v-list-item-avatar color="grey">
-                        <img src="https://randomuser.me/api/portraits/men/1.jpg">
-                    </v-list-item-avatar>
-                    <v-list-item-content>
-                        <v-list-item-title>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</v-list-item-title>
-                        <v-list-item-subtitle>by Leanne Graham</v-list-item-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-card-text>quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto</v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-
-                    <v-btn icon>
-                        <v-icon>mdi-heart</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                        <v-icon>mdi-bookmark</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                        <v-icon>mdi-share-variant</v-icon>
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-            
-            <v-card max-width="1000px" class="text-left pa-3 mb-8">
-                <v-list-item>
-                    <v-list-item-avatar color="grey">
-                        <img src="https://randomuser.me/api/portraits/men/1.jpg">
-                    </v-list-item-avatar>
-                    <v-list-item-content>
-                        <v-list-item-title>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</v-list-item-title>
-                        <v-list-item-subtitle>by Leanne Graham</v-list-item-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-card-text>quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto</v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-
-                    <v-btn icon>
-                        <v-icon>mdi-heart</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                        <v-icon>mdi-bookmark</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                        <v-icon>mdi-share-variant</v-icon>
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'Profile',
+    data(){
+        return {
+            posts: [],
+            userData: {
+                name: '',
+                website: '',
+                email: '',
+                address: '',
+                company: {
+                    name: ''
+                }
+            }
+        } 
+    },
+    methods: {
+        getPosts(){
+            this.axios.get('http://188.225.47.187/api/jsonplaceholder.php?endpoint=posts?userId=' + this.$route.params.id)
+                .then((response)=>{
+                    this.posts = response.data;
+                })
+        },
+        getUserData(){
+            this.axios.get('http://188.225.47.187/api/jsonplaceholder.php?endpoint=users/' + this.$route.params.id)
+                .then((response)=>{
+                    this.userData = response.data;
+                    window.console.log(this.userData);
+                })
+        }
+    },
+    mounted(){
+        this.getPosts();
+        this.getUserData();
+    },
+    watch: {
+        $route() {
+            this.getPosts();
+            this.getUserData();
+        }
+    }
+}
+</script>
