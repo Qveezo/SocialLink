47e3c2e09f8054bcf2bf3f13c0e84bad1eb0915b
Ruslan Abkadirov <rusabk@yandex.ru>
2020-01-17 04:56:23 +0300

    [additional] axios & vue-axios installed

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import vuetify from './plugins/vuetify';
 import router from './router'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
 Vue.config.productionTip = false
 
 new Vue({
